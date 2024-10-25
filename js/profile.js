8094fb24b82644c2f735aef21f844efe12f8f062
BaimYow <baimyow1999@gmail.com>
2024-10-25 22:31:19 +0700

    penyempurnaan

--- js/profile.js
+++ js/profile.js
@@ -9,16 +9,3 @@ document.querySelector("#submit-register").onclick = (e) => {
 document.querySelector("#close-notif").onclick = (e) => {
   notif.classList.remove("active");
 };
-
-// ketika cart di klik lalu muncul keranjang
-const cart = document.querySelector(".keranjang");
-
-document.querySelector(".cart-button").onclick = (e) => {
-  cart.classList.toggle("active");
-  bb.classList.toggle("blur");
-};
-
-document.querySelector(".back-close").onclick = (e) => {
-  cart.classList.remove("active");
-  bb.classList.remove("blur");
-};
